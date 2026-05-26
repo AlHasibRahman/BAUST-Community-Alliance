4b75bc8fea69f62c4d844ce0360fed7f7b3be682
AlHasibRahman <rahmanalhasibur3@gmail.com>
2026-05-26 09:25:50 +0600

    Changes modify in routes.tsx file

--- BAUST-community-Alliance-React/src/Routes/routes.tsx
+++ BAUST-community-Alliance-React/src/Routes/routes.tsx
@@ -3,7 +3,9 @@ import Layout from "../Components/Layout";
 import AboutUs from "../Components/AboutUs";
 import HomePage from "../Components/HomePage";
 import BlogPage from "../Components/BlogPage";
+import StoriesPage from "../Components/StoriesPage";
 import CreateContent from "../Components/CreateContent";
+import PublicationPage from "../Components/PublicationPage"
 
 export default function routes() {
     const router = createBrowserRouter([
@@ -13,7 +15,9 @@ export default function routes() {
                 { path: "", element: <HomePage /> },
                 { path: "admin/createContent", element: <CreateContent/>},
                 { path: "about", element: <AboutUs /> },
-                { path: "category/blog", element: <BlogPage />}
+                { path: "category/blog", element: <BlogPage />},
+                { path: "category/story", element: <StoriesPage />},
+                { path: "category/publication", element: <PublicationPage />}
             ]
         }
     ]);
